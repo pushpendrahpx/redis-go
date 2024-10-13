13095f4e20f20c2e25043476ced0de731cc7ff26
pushpendra-factors <actions@github.com>
2024-10-13 11:27:36 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -10,6 +10,33 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+
+func do(c net.Conn) {
+
+	defer closeConnection(c)
+
+
+	buf := make([]byte, 128)
+
+	n, err := c.Read(buf)
+	fmt.Printf("Read %d chars\n", n)
+	if err != nil {
+		fmt.Errorf("Got Error : %v", err)
+	}
+
+	fmt.Println(buf, string(buf))
+
+	n, err = c.Write([]byte("+PONG\r\n"))
+
+	fmt.Printf("Wrote %d chars\n", n)
+	fmt.Println(c.RemoteAddr())
+	if err != nil {
+		fmt.Errorf("Got Error while writing : %v", err)
+	}
+	
+
+
+}
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -21,9 +48,28 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	for {
+
+		
+		// This is a Blcking Call
+		c, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}else{
+			go do(c)
+		}
+	}
+}
+
+
+func closeConnection(c net.Conn) {
+	err := c.Close()
+
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Errorf("Error: %v", err)
+		panic("Connection Closed")
 	}
 }
